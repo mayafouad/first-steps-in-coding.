e0c06f7d9e2728c6852e6cbd12187d468822c869
Maya Fouad <112335115+mayafouad@users.noreply.github.com>
2023-09-23 20:00:56 +0300

    Vending_Machine.cpp

---
+++ Vending_Machine.cpp
@@ -0,0 +1,72 @@
+#include "Vending_Machine.h"
+#include <bits/stdc++.h>
+#define ll long long 
+#define fast() ios_base::sync_with_stdio(false),cin.tie(NULL);
+#define el "\n"
+#define testcase int t; cin>>t; while(t--)
+using namespace std;
+
+int Water::get_Water(){return q;}
+
+int Water::Water_price(){return p;}
+
+void Water::buy(){q--;}
+
+int Cola::get_Cola(){return q;}
+
+int Cola::cola_price(){return p;}
+
+void Cola::buy(){q--;}
+            
+int Pepsi::get_Pepsi(){return q;}
+            
+int Pepsi::pepsi_price(){return p;}
+            
+void Pepsi::buy(){q--;}        
+        
+int Chips::get_Chips(){return q;}            
+        
+int Chips::chips_price(){return p;}       
+        
+void Chips::buy(){q--;}            
+        
+int Pringles::get_Pringles(){return q;}
+
+int Pringles::pringles_price(){return p;}            
+        
+void Pringles::buy(){q--;}            
+        
+int Marshmallow::get_Marshmallow(){return q;}        
+            
+int Marshmallow::marsh_price(){return p;}        
+
+void Marshmallow::buy(){q--;}
+        
+int Chocolate::get_Chocolate(){return q;}
+            
+int Chocolate::choco_price(){return p;}        
+        
+void Chocolate::buy(){q--;}            
+        
+int Doritos::get_Doritos(){return q;}        
+            
+int Doritos::doritos_price(){return p;}        
+
+void Doritos::buy(){q--;}
+        
+int Ice_Coffee::get_Ice_Coffee(){return q;}
+            
+int Ice_Coffee::ice_price(){return p;}        
+        
+void Ice_Coffee::buy(){q--;}            
+        
+int Strawberry_milk::get_Strawberry_milk(){return q;}        
+            
+int Strawberry_milk::milk_price(){return p;}
+                           
+void Strawberry_milk::buy(){q--;}
+
+        
+            
+        
+        
